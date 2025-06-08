64f5f5a9294e453a42082fd352a8f4c6cec008ff
lonezu03 <118800029+lonezu03@users.noreply.github.com>
2025-06-08 10:54:05 +0700

    8/6/2025

--- adminTruyen/src/redux/rootReducer.jsx
+++ adminTruyen/src/redux/rootReducer.jsx
@@ -4,11 +4,13 @@ import novelReducer from './novelSlice';
 import authorReducer from './authorSlice';
 import categoryReducer from './categorySlice';
 import chapterReducer from './chapterSlice';
+import userReducer from './userSlice';
 const rootReducer = combineReducers({
   novels: novelReducer,
   authors: authorReducer,
   categories: categoryReducer,
-  chapters: chapterReducer
+  chapters: chapterReducer,
+  user: userReducer,
 });
 
 export default rootReducer;
